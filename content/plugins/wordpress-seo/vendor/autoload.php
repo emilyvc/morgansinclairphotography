6538c38ec108ae252a2ecc8f52b3e9ec5a697948
Emily Cressey <hello@emily.vc>
2017-03-20 13:48:13 +0000

    update plugins

--- content/plugins/wordpress-seo/vendor/autoload.php
+++ content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInita2231c82309a04c3b7d7679bf9a57481::getLoader();
+return ComposerAutoloaderInitb302bc72d072431a8a4fd6aae5897830::getLoader();
--- content/plugins/wordpress-seo/vendor/autoload_52.php
+++ content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitf910e8577b73fe172cf6b24cb2405a15::getLoader();
+return ComposerAutoloaderInita28c17fcbbdfaa3b59475992c1d59b05::getLoader();
